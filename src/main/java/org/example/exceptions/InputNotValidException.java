a078a11b1be9f856320b93f60dba65172b108f23
BN2002 <doanhcms@gmail.com>
2023-12-12 13:52:39 +0700

    Thêm validation input

--- src/main/java/org/example/exceptions/InputNotValidException.java
+++ src/main/java/org/example/exceptions/InputNotValidException.java
@@ -1,2 +1,15 @@
-package org.example.exceptions;public class InputNotValidException {
+package org.example.exceptions;
+
+import org.example.dtos.InputErrorDto;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InputNotValidException extends RuntimeException{
+    public ArrayList<InputErrorDto> errors;
+    public InputNotValidException(String errorMessage, ArrayList<InputErrorDto> errors) {
+        super(errorMessage);
+        this.errors = errors;
+    }
+
 }
--- src/main/java/org/example/utils/ValidationUtil.java
+++ src/main/java/org/example/utils/ValidationUtil.java
@@ -1,2 +1,27 @@
-package org.example.utils;public class ValidationUtil {
+package org.example.utils;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.example.dtos.InputErrorDto;
+import org.example.exceptions.InputNotValidException;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class ValidationUtil {
+    public static <T> void runValidation(T object) {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        Validator validator = validatorFactory.getValidator();
+        Set<ConstraintViolation<T>> violations = validator.validate(object);
+        ArrayList<InputErrorDto> errors = new ArrayList<>();
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<T> violation : violations) {
+                InputErrorDto error = new InputErrorDto(violation.getPropertyPath().toString(), violation.getMessage());
+                errors.add(error);
+            }
+            throw new InputNotValidException("Validation error occurred", errors);
+        }
+    }
 }
